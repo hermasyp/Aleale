4b8037821b9f751d9ef9d00a69ffdb57645fcd58
Muhammad Hermas Yuda P <hermas.yuda@gmail.com>
2022-09-02 21:10:03 +0700

    Finishing feature print header

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,20 @@
 class App {
+    fun printHeader(){
+        println("""
+            ============================
+            Aplikasi "Apakah saya jomblo"
+            ============================
+        """.trimIndent())
+    }
+
+    fun run(){
+        printHeader()
+    }
 
     companion object{
         @JvmStatic
         fun main(args: Array<String>) {
-
+            App().run()
         }
     }
 }
